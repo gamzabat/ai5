1a69258b5302d51479dee05ce832185118c5f92b
ai5Chobo <soogamza@gmail.com>
2024-07-24 12:17:24 +0900

    2024-0724

--- keras/keras22_softmax3_fetch_covtype.py
+++ keras/keras22_softmax3_fetch_covtype.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-from sklearn.datasets import load_wine
+from sklearn.datasets import fetch_covtype
 
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
@@ -13,18 +13,20 @@ from sklearn.metrics import accuracy_score
 import time
 
 #1 data
-dataset = load_wine()
+dataset = fetch_covtype()
 
-x = dataset.data
+x = pd.DataFrame(dataset.data).iloc[:, :13]
+
 y = pd.get_dummies(dataset.target)
 
 print(x)
+print(y)
 
 x_train, x_test, y_train, y_test = train_test_split(
     x,
     y,
-    train_size = 0.8,
-    random_state = 5555
+    train_size = 0.9,
+    random_state = 3333
 )
 
 #2 model
@@ -42,8 +44,9 @@ model.add(Dense(64, activation = 'relu'))
 model.add(Dense(64, activation = 'relu'))
 model.add(Dense(64, activation = 'relu'))
 model.add(Dense(64, activation = 'relu'))
+model.add(Dense(64, activation = 'relu'))
 
-model.add(Dense(3, activation = 'softmax'))
+model.add(Dense(7, activation = 'softmax'))
 
 #3 compile
 model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
@@ -63,12 +66,15 @@ model.fit(
     validation_split = 0.3,
     callbacks = [es],
     epochs = 1000,
-    batch_size = 4,
-    verbose = 2
+    batch_size = 1024,
+    verbose = 1
 )
 
 end_time = time.time()
 
+print("fit time :", round(end_time - start_time, 2), "초")
+
+#4 predict
 loss = model.evaluate(x_test, y_test, verbose = 0)
 
 y_pred = model.predict(x_test)
@@ -77,6 +83,7 @@ print(np.round(y_pred[:10]))
 
 print("acc :", accuracy_score(y_test, np.round(y_pred)))
 
-# acc : 0.8333333333333334
-# acc : 0.9074074074074074
-# acc : 0.9259259259259259
+# acc : 0.8836690963228144
+# acc : 0.8947703587687065
+# acc : 0.9113283535850745
+# acc : 0.925252142783381
---
+++ keras/keras22_softmax4_digits.py
@@ -0,0 +1,6 @@
+from sklearn.datasets import load_digits
+
+x, y = load_digits(return_X_y = True)
+
+print(x)
+print(y)
